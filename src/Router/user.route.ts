72c2a438275102c3bb358559959e6ecdf6634c38
Darahaas N <65232365+Darahaas2001@users.noreply.github.com>
2022-12-11 18:49:13 +0530

    minor bug fix

--- src/Router/user.route.ts
+++ src/Router/user.route.ts
@@ -3,8 +3,9 @@ import { signup, updateUser } from '../Controller/user.controller';
 import { login, protect } from '../Controller/auth.controller';
 
 let router = Router();
-router.route('/:teamNo').patch(protect, updateUser);
+
 router.route('/login').post(login);
 router.route('/signup').post(signup);
+router.route('/:teamNo').patch(protect, updateUser);
 
 export default router;
